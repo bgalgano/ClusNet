12bb05d2cdb44cf4f9008a0a08ab587b711a9981
Brianna Galgano <erebor@briannas-mbp.home>
2021-05-19 10:34:28 -0400

    test

--- code/modules/ClusNet/Cluster.py
+++ code/modules/ClusNet/Cluster.py
@@ -92,7 +92,7 @@ class Cluster:
         shift cluster randomly within bounds of image
         """
         
-        low = -1*delta
+        low = -1 * delta
         high = delta
         
         r = self.Rpixel
